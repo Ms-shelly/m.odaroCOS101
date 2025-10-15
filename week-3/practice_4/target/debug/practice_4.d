5f1b074d0b506b00597dac4734506bf75c0567b2
Ms-shelly <michelle.odaro@pau.edu.ng>
2025-10-15 17:08:18 +0100

    (My third commit)

---
+++ week-3/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\Michelle\Documents\m.odaroCOS101\week-3\practice_4\target\debug\practice_4.exe: C:\Users\Michelle\Documents\m.odaroCOS101\week-3\practice_4\src\main.rs
---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\Michelle\Documents\m.odaroCOS101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\Michelle\Documents\m.odaroCOS101\week-3\practice_5\src\main.rs
